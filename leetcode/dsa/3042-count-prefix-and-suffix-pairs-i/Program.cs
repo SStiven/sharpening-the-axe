a21efa642e61197622e3689a512a5e58e31fa9df
Stiven Villegas <stiven_villegas@epam.com>
2025-05-19 13:28:51 -0500

    Solves the count prefix and suffix pairs i problem

---
+++ leetcode/dsa/3042-count-prefix-and-suffix-pairs-i/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CountPrefixAndSuffixPairsI;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var s = new Solution();
+        var c = s.CountPrefixSuffixPairs(["a", "aba", "ababa", "aa"]);
+        Console.WriteLine(c);
+    }
+}
---
+++ leetcode/dsa/3042-count-prefix-and-suffix-pairs-i/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace CountPrefixAndSuffixPairsI;
+
+public class Solution
+{
+    public int CountPrefixSuffixPairs(string[] words)
+    {
+        int count = 0;
+
+        for (int j = 1; j < words.Length; j++)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                if (IsPrefixAndSuffix(words[i], words[j]))
+                {
+                    count += 1;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private bool IsPrefixAndSuffix(string left, string right)
+    {
+        if (left.Length > right.Length)
+        {
+            return false;
+        }
+
+        return right.StartsWith(left) && right.EndsWith(left);
+    }
+}
