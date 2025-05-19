4dee278372414a8db7d237b31ab21faeee1f2af4
Stiven Villegas <stiven_villegas@epam.com>
2025-05-19 16:27:59 -0500

    Solves 3318. Find X-Sum of All K-Long Subarrays I

---
+++ leetcode/dsa/3318-find-x-sum-of-all-k-long-subarrays-i/FindXSumOfAllKLongSubarraysI/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FindXSumOfAllKLongSubarraysI;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var s = new Solution();
+        var lcs = s.FindXSum([1, 1, 2, 2, 3, 4, 2, 3], 6, 2);
+        Console.WriteLine(lcs);
+
+    }
+}
---
+++ leetcode/dsa/3318-find-x-sum-of-all-k-long-subarrays-i/FindXSumOfAllKLongSubarraysI/Solution.cs
@@ -0,0 +1,61 @@
+﻿namespace FindXSumOfAllKLongSubarraysI;
+
+public class Solution
+{
+    public int[] FindXSum(int[] nums, int k, int x)
+    {
+
+        int n = nums.Length;
+        var result = new List<int>();
+
+        var numToFrecuency = new Dictionary<int, int>();
+
+        for (int i = 0; i < k; i++)
+        {
+            if (!numToFrecuency.ContainsKey(nums[i]))
+            {
+                numToFrecuency[nums[i]] = 1;
+            }
+            else
+            {
+                numToFrecuency[nums[i]] += 1;
+            }
+        }
+
+        result.Add(CalcXSum(numToFrecuency, x));
+
+        for (int i = k; i < n; i++)
+        {
+            int outNum = nums[i - k];
+            int inNum = nums[i];
+
+            numToFrecuency[outNum] -= 1;
+            if (numToFrecuency[outNum] == 0)
+            {
+                numToFrecuency.Remove(outNum);
+            }
+
+            if (!numToFrecuency.ContainsKey(inNum))
+            {
+                numToFrecuency[inNum] = 1;
+            }
+            else
+            {
+                numToFrecuency[inNum] += 1;
+            }
+
+            result.Add(CalcXSum(numToFrecuency, x));
+        }
+
+        return result.ToArray();
+    }
+
+    private int CalcXSum(Dictionary<int, int> numToFrecuency, int x)
+    {
+        return numToFrecuency
+            .OrderByDescending(p => p.Value)
+            .ThenByDescending(p => p.Key)
+            .Take(x)
+            .Sum(p => p.Key * p.Value);
+    }
+}
