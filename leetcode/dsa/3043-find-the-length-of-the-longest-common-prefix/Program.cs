33cd86bd9711d0ccf0f1bef229eba7690b0a913a
Stiven Villegas <stiven_villegas@epam.com>
2025-05-19 15:25:32 -0500

    time limit exceeded for 3043

---
+++ leetcode/dsa/3043-find-the-length-of-the-longest-common-prefix/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FindTheLengthOfTheLongestCommonPrefix;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Time Limit Exceeded
+        var s = new Solution();
+        //Works
+        //var length = s.LongestCommonPrefix([1, 10, 100,1000], [1000]);
+
+
+        //var length = s.LongestCommonPrefix([5], [33]);
+
+        //If digit is zero can have problems
+        var length = s.LongestCommonPrefix([4, 1], [40, 20]);
+        Console.WriteLine(length);
+
+    }
+}
---
+++ leetcode/dsa/3043-find-the-length-of-the-longest-common-prefix/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace FindTheLengthOfTheLongestCommonPrefix;
+
+public class Solution
+{
+    public int LongestCommonPrefix(int[] arr1, int[] arr2)
+    {
+        int longest = 0;
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            for (int j = 0; j < arr2.Length; j++)
+            {
+                var prefixLength = LongestCommonPrefix(arr1[i], arr2[j]);
+                longest = Math.Max(longest, prefixLength);
+            }
+        }
+
+        return longest;
+    }
+
+    private int LongestCommonPrefix(int num1, int num2)
+    {
+        int numDigits1 = (int)Math.Floor(Math.Log10(num1)) + 1;
+        int numDigits2 = (int)Math.Floor(Math.Log10(num2)) + 1;
+
+        var minNumDigits = Math.Min(numDigits1, numDigits2);
+        int count = 0;
+        for (int i = 0; i < minNumDigits; i++)
+        {
+            int digit1 = GetDigitFromLeft(num1, i);
+            int digit2 = GetDigitFromLeft(num2, i);
+            if (digit1 != digit2)
+            {
+                return count;
+            }
+
+            count += 1;
+        }
+
+        return count;
+    }
+
+    private int GetDigitFromLeft(int num, int position)
+    {
+        int digits = (int)Math.Floor(Math.Log10(num)) + 1;
+
+        int power = digits - position - 1;
+        int divisor = (int)Math.Pow(10, power);
+        int digit = (num / divisor) % 10;
+        return digit;
+    }
+}
