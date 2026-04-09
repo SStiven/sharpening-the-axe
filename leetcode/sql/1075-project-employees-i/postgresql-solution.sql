3d8faf7ca327d8e040dca15bb60cd2e9f8fb0832
Stiven Villegas <sstiven@protonmail.com>
2026-04-09 13:31:51 -0500

    Solves LeetCode SOL 1075. Project Employees I

--- leetcode/sql/1075-project-employees-i/postgresql-solution.sql
+++ leetcode/sql/1075-project-employees-i/postgresql-solution.sql
@@ -4,3 +4,32 @@ from  Project p
 join Employee e on p.employee_id = e.employee_id
 group by p.project_id
 order by p.project_id asc
+
+
+-- Improved version
+select
+    p.project_id
+    ,round(AVG(experience_years),2) as average_years 
+from project p
+left join employee e using(employee_id)
+group by p.project_id
+
+
+CREATE TABLE Employee (
+    employee_id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    experience_years INTEGER NOT NULL
+);
+
+CREATE TABLE Project (
+    project_id INTEGER,
+    employee_id INTEGER,
+    PRIMARY KEY (project_id, employee_id),
+    FOREIGN KEY (employee_id) REFERENCES Employee (employee_id)
+);
+
+INSERT INTO Employee (employee_id, name, experience_years) VALUES
+(1, 'Khaled', 3),
+(2, 'Ali', 2),
+(3, 'John', 1),
+(4, 'Doe', 2);
